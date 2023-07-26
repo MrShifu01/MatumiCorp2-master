004113a940db4347dc275ec15c1d4e657751f3bc
MrShifu01 <stander.christian.com>
2023-07-26 10:47:45 +0200

    a

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,8 +4,8 @@ import Footer from './components/Footer';
 import HomePage from './pages/HomePage';
 import axios from 'axios'
 
-axios.defaults.baseURL = "http://localhost:8000"
-// axios.defaults.baseURL = "https://matumi-server.onrender.com"
+// axios.defaults.baseURL = "http://localhost:8000"
+axios.defaults.baseURL = "https://matumi-server.onrender.com"
 
 function App() {
   return (
